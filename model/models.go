6a68b7db7083b1c8665fe1545963f84ffb5184e0
NooBMasteR <abbasasadi.13a77@gmail.com>
2025-05-20 19:15:18 +0330

    chore(model): sync model.go with master branch

--- model/models.go
+++ model/models.go
@@ -8,6 +8,8 @@ type Recipe struct {
 	User        User         `gorm:"foreignKey:UserID" json:"-"`
 	Ingridients []Ingridient `gorm:"foreignKey:RecipeID" json:"ingridients"`
 	Comments    []Comment    `gorm:"foreignKey:RecipeID" json:"comments"`
+	Favorited   []Favorite   `gorm:"foreignKey: RecipeID" json:"favorites"`
+	Ratings     []Rating     `gorm:"foreignKey:RecipeID" json:"ratings"`
 }
 
 type Ingridient struct {
@@ -18,14 +20,16 @@ type Ingridient struct {
 }
 
 type User struct {
-	ID       uint      `gorm:"primaryKey"`
-	Name     string    `json:"name"`
-	LastName string    `json:"last_name"`
-	Salt     string    `json:"salt"`
-	Password string    `json:"password"`
-	Email    string    `json:"email"`
-	Comments []Comment `gorm:"foreignKey: UserID" json:"comments"`
-	Recipes  []Recipe  `gorm:"foreignKey: UserID" json:"recipes"`
+	ID        uint       `gorm:"primaryKey"`
+	Name      string     `json:"name"`
+	LastName  string     `json:"last_name"`
+	Salt      string     `json:"salt"`
+	Password  string     `json:"password"`
+	Email     string     `json:"email"`
+	Comments  []Comment  `gorm:"foreignKey: UserID" json:"comments"`
+	Recipes   []Recipe   `gorm:"foreignKey: UserID" json:"recipes"`
+	Favorites []Favorite `gorm:"foreignKey: UserID" json:"favorites"`
+	Ratings   []Rating   `gorm:"foreignKey:UserID" json:"ratings"`
 }
 
 type Comment struct {
@@ -35,4 +39,17 @@ type Comment struct {
 	Likes       uint   `gorm:"defualt:0" json:"likes"`
 	UserID      uint   `json:"user_id"`
 	RecipeID    uint   `json:"recipe_id"`
+}
+
+type Favorite struct {
+	ID       uint `gorm:"primaryKey"`
+	UserID   uint `json:"user_id" binding:"required"`
+	RecipeID uint `json:"recipe_id" binding:"required"`
+}
+
+type Rating struct {
+	ID       uint `gorm:"primaryKey"`
+	RecipeID uint `json:"recipe_id"`
+	UserID   uint `json:"user_id"`
+	Score    uint `gorm:"check:score >= 1 AND score <= 5" json:"score" binding:"required"`
 }
