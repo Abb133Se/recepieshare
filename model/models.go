db376ff3c523a0b9a94046cdf1272474dc8acba5
NooBMasteR <abbasasadi.13a77@gmail.com>
2025-08-10 21:52:26 +0330

    refactor(api): DB error fix and response refactor - Fixed DB date/time error while calling DeleteRecipeHandler - Ommiting unnecessary fields in GetAllRecipesHandler (ingeredients, comments, etc) - Adding pagination to GetAllRecipeCategoriesHandler - Ommiting "ingredients" field in GetSearchRecipesHnadler response

--- model/models.go
+++ model/models.go
@@ -7,14 +7,14 @@ type Recipe struct {
 	Title       string       `json:"title" binding:"required"`
 	Text        string       `json:"text" binding:"required"`
 	UserID      uint         `json:"user_id" binding:"required"`
-	User        User         `gorm:"foreignKey:UserID" json:"-"`
-	Ingredients []Ingredient `gorm:"foreignKey:RecipeID" json:"ingredients"`
-	Comments    []Comment    `gorm:"foreignKey:RecipeID" json:"comments"`
-	Favorited   []Favorite   `gorm:"foreignKey: RecipeID" json:"favorites"`
-	Ratings     []Rating     `gorm:"foreignKey:RecipeID" json:"ratings"`
-	Tags        []Tag        `gorm:"many2many:recipe_tags" json:"tags"`
-	Categories  []Category   `gorm:"many2many:recipe_categories" json:"categories"`
-	Steps       []Step       `gorm:"foreignKey:RecipeID" json:"steps"`
+	User        User         `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"-"`
+	Ingredients []Ingredient `gorm:"foreignKey:RecipeID;constraint:OnDelete:CASCADE" json:"ingredients"`
+	Comments    []Comment    `gorm:"foreignKey:RecipeID;constraint:OnDelete:CASCADE" json:"comments"`
+	Favorited   []Favorite   `gorm:"foreignKey:RecipeID;constraint:OnDelete:CASCADE" json:"favorites"`
+	Ratings     []Rating     `gorm:"foreignKey:RecipeID;constraint:OnDelete:CASCADE" json:"ratings"`
+	Tags        []Tag        `gorm:"many2many:recipe_tags;constraint:OnDelete:CASCADE" json:"tags"`
+	Categories  []Category   `gorm:"many2many:recipe_categories;constraint:OnDelete:CASCADE" json:"categories"`
+	Steps       []Step       `gorm:"foreignKey:RecipeID;constraint:OnDelete:CASCADE" json:"steps"`
 	CreatedAt   time.Time    `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time    `gorm:"autoUpdateTime"`
 }
@@ -23,7 +23,7 @@ type Ingredient struct {
 	ID        uint      `gorm:"primaryKey"`
 	Name      string    `json:"name" binding:"required"`
 	Amount    string    `json:"amount" binding:"required"`
-	RecipeID  uint      `json:"recipe_id"`
+	RecipeID  uint      `json:"recipe_id" gorm:"index"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
@@ -46,10 +46,10 @@ type User struct {
 	Email                  string     `json:"email"`
 	PasswordResetToken     string     `json:"-" gorm:"size:255"`
 	PasswordResetExpiresAt *time.Time `json:"-"`
-	Comments               []Comment  `gorm:"foreignKey: UserID" json:"comments"`
-	Recipes                []Recipe   `gorm:"foreignKey: UserID" json:"recipes"`
-	Favorites              []Favorite `gorm:"foreignKey: UserID" json:"favorites"`
-	Ratings                []Rating   `gorm:"foreignKey:UserID" json:"ratings"`
+	Comments               []Comment  `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"comments"`
+	Recipes                []Recipe   `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"recipes"`
+	Favorites              []Favorite `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"favorites"`
+	Ratings                []Rating   `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"ratings"`
 	CreatedAt              time.Time  `gorm:"autoCreateTime"`
 	UpdatedAt              time.Time  `gorm:"autoUpdateTime"`
 }
@@ -59,24 +59,24 @@ type Comment struct {
 	Title       string    `json:"title" binding:"required"`
 	Description string    `json:"description" binding:"required"`
 	Likes       int       `gorm:"default:0" json:"likes"`
-	UserID      uint      `json:"user_id"`
-	RecipeID    uint      `json:"recipe_id"`
+	UserID      uint      `json:"user_id" gorm:"index"`
+	RecipeID    uint      `json:"recipe_id" gorm:"index"`
 	CreatedAt   time.Time `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `gorm:"autoUpdateTime"`
 }
 
 type Favorite struct {
 	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `json:"user_id" binding:"required"`
-	RecipeID  uint      `json:"recipe_id" binding:"required"`
+	UserID    uint      `json:"user_id" binding:"required" gorm:"index"`
+	RecipeID  uint      `json:"recipe_id" binding:"required" gorm:"index"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
 
 type Rating struct {
 	ID        uint      `gorm:"primaryKey"`
-	RecipeID  uint      `json:"recipe_id"`
-	UserID    uint      `json:"user_id"`
+	RecipeID  uint      `json:"recipe_id" gorm:"index"`
+	UserID    uint      `json:"user_id" gorm:"index"`
 	Score     uint      `gorm:"check:score >= 1 AND score <= 5" json:"score" binding:"required"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
@@ -85,7 +85,7 @@ type Rating struct {
 type Tag struct {
 	ID        uint      `gorm:"primaryKey"`
 	Name      string    `json:"name" binding:"required" gorm:"unique;not null"`
-	Recipes   []Recipe  `gorm:"many2many:recipe_tags" json:"-"`
+	Recipes   []Recipe  `gorm:"many2many:recipe_tags;constraint:OnDelete:CASCADE" json:"-"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
@@ -93,7 +93,7 @@ type Tag struct {
 type Category struct {
 	ID        uint      `gorm:"primaryKey"`
 	Name      string    `json:"name" binding:"required" gorm:"unique;not null"`
-	Recipes   []Recipe  `gorm:"many2many:recipe_categories" json:"-"`
+	Recipes   []Recipe  `gorm:"many2many:recipe_categories;constraint:OnDelete:CASCADE" json:"-"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
